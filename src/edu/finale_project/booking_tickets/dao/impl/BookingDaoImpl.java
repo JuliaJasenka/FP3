8f32f0003c7f36d8f9ab1ea5679e29b03d27a4d2
JuliaJasenka <148490193+JuliaJasenka@users.noreply.github.com>
2024-03-08 09:22:47 +0300

    Add files via upload

---
+++ src/edu/finale_project/booking_tickets/dao/impl/BookingDaoImpl.java
@@ -0,0 +1,126 @@
+package edu.finale_project.booking_tickets.dao.impl;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.finale_project.booking_tickets.dao.BookingDao;
+import edu.finale_project.booking_tickets.dao.DaoException;
+import edu.finale_project.booking_tickets.dao.DaoHelper;
+import edu.finale_project.booking_tickets.entity.Booking;
+
+public class BookingDaoImpl implements BookingDao {
+
+	private static final String DELIMITER = ";";
+	private static final String FILE_NAME = "booking.txt";
+
+	@Override
+	public String add(Booking booking) throws DaoException {
+		try {
+			DaoHelper.writeFile(prepareForStoring(booking) + '\n', FILE_NAME, true);
+			return booking.getId();
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось сохранить бронь (ID - " + booking.getId() + ". " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public Booking findById(String id) throws DaoException {
+		try {
+			List<String> bookingList = DaoHelper.readFileByLine(FILE_NAME);
+			for (String booking : bookingList) {
+				if (booking.startsWith(id)) {
+					return convertFromString(booking);
+				}
+			}
+			throw new DaoException("Бранирование с ID - " + id + " не найден");
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось найти бронь (ID - " + id + ". " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public void update(Booking booking) throws DaoException {
+		try {
+			List<String> bookingList = DaoHelper.readFileByLine(FILE_NAME);
+			for (int i = 0; i < bookingList.size(); i++) {
+				if (bookingList.get(i).startsWith(booking.getId())) {
+					bookingList.set(i, prepareForStoring(booking));
+				}
+			}
+			String updatedPayload = "";
+			for (String line : bookingList) {
+				if (!line.isEmpty()) {
+					updatedPayload += line + '\n';
+				}
+			}
+			DaoHelper.writeFile(updatedPayload, FILE_NAME);
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось обновить бронь (ID - " + booking.getId() + ". " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public boolean delete(String id) throws DaoException {
+		try {
+			List<String> bookingList = DaoHelper.readFileByLine(FILE_NAME);
+			List<String> newBookingList = new ArrayList<>();
+			boolean isRemoved = false;
+			for (String booking : bookingList) {
+				if (booking.startsWith(id)) {
+					isRemoved = true;
+				} else {
+					newBookingList.add(booking);
+				}
+			}
+			if (isRemoved) {
+				String updatedPayload = "";
+				for (String line : newBookingList) {
+					updatedPayload += line + '\n';
+				}
+				DaoHelper.writeFile(updatedPayload, FILE_NAME);
+			}
+			return isRemoved;
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось удалить бронь (ID - " + id + ". " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public List<Booking> getBookingInfoByUserId(int id) throws DaoException {
+		try {
+			List<String> bookingList = DaoHelper.readFileByLine(FILE_NAME);
+			List<Booking> filteredBookingList = new ArrayList<>();
+			for (String bookingStr : bookingList) {
+				Booking booking = convertFromString(bookingStr);
+				if (id == booking.getUserId()) {
+					filteredBookingList.add(booking);
+				}
+			}
+
+			return filteredBookingList;
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось найти бронь (ID - " + id + ". " + ex.getMessage());
+		}
+	}
+
+	private String prepareForStoring(Booking booking) {
+		return booking.getId() + DELIMITER + booking.getTrainId() + DELIMITER + booking.getUserId() + DELIMITER
+				+ booking.getBookingDate() + DELIMITER + booking.getSeatsNumber() + DELIMITER;
+	}
+
+	private Booking convertFromString(String bookingStr) {
+		if (bookingStr == null || bookingStr.isEmpty())
+			return null;
+		Booking booking = new Booking();
+		String[] split = bookingStr.split(DELIMITER);
+		booking.setId(split[0]);
+		booking.setTrainId(split[1]);
+		booking.setUserId(Integer.parseInt(split[2]));
+		booking.setBookingDate(LocalDateTime.parse(split[3]));
+		booking.setSeatsNumber(Integer.parseInt(split[4]));
+
+		return booking;
+	}
+}
--- src/edu/finale_project/booking_tickets/dao/impl/TrainDaoImpl.java
+++ src/edu/finale_project/booking_tickets/dao/impl/TrainDaoImpl.java
@@ -1,1 +1,177 @@
+package edu.finale_project.booking_tickets.dao.impl;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import edu.finale_project.booking_tickets.dao.DaoException;
+import edu.finale_project.booking_tickets.dao.DaoHelper;
+import edu.finale_project.booking_tickets.dao.TrainDao;
+import edu.finale_project.booking_tickets.entity.Train;
+
+public class TrainDaoImpl implements TrainDao {
+
+	private static final String DELIMITER = ";";
+	private static final String FILE_NAME = "train.txt";
+
+	@Override
+	public String add(Train entity) throws DaoException {
+		try {
+			DaoHelper.writeFile(prepareForStoring(entity), FILE_NAME, true);
+			return entity.getId();
+		} catch (IOException ex) {
+			throw new DaoException("Failed to store the train (ID - " + entity.getId() + ". " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public Train findById(String id) throws DaoException {
+		try {
+			List<String> trainList = DaoHelper.readFileByLine(FILE_NAME);
+			for (String train : trainList) {
+				if (train.startsWith(id)) {
+					return convertFromString(train);
+				}
+			}
+			throw new DaoException("Маршурт с ID - " + id + " не найден");
+		} catch (IOException ex) {
+			throw new DaoException("Не удалость сохранить маршрут (ID - " + id + ". " + ex.getMessage());
+		}
+	}
+
+	 @Override
+	    public void update(Train updatedTrain) throws DaoException {
+	        try {
+	            List<String> trainStrList = DaoHelper.readFileByLine(FILE_NAME);
+	            List<Train> trainList = new ArrayList<>();
+	            for (String line : trainStrList) {
+	                trainList.add(convertFromString(line));
+	            }
+	            for (Train train : trainList) {
+	                if (train.getId().equals(updatedTrain.getId())) {
+	                    if (updatedTrain.getTrainNumber() != null) {
+	                        train.setTrainNumber(updatedTrain.getTrainNumber());
+	                    }
+	                    if (updatedTrain.getTo() != null) {
+	                        train.setTo(updatedTrain.getTo());
+	                    }
+	                    if (updatedTrain.getFrom() != null) {
+	                        train.setFrom(updatedTrain.getFrom());
+	                    }
+	                    if (updatedTrain.getDepartureTime() != null) {
+	                        train.setDepartureTime(updatedTrain.getDepartureTime());
+	                    }
+	                    if (updatedTrain.getFreeSeats() != null) {
+	                        train.setFreeSeats(updatedTrain.getFreeSeats());
+	                    }
+	                }
+	            }
+	            String updatedPayload = "";
+	            for (Train train : trainList) {
+	                updatedPayload += prepareForStoring(train) + '\n';
+	            }
+	            DaoHelper.writeFile(updatedPayload, FILE_NAME);
+	        } catch (IOException ex) {
+	            throw new DaoException("Не удалось обновить маршурт (ID - " + updatedTrain.getId() + ". " + ex.getMessage());
+	        }
+	    }
+
+
+	@Override
+	public boolean delete(String id) throws DaoException {
+		try {
+			List<String> trainList = DaoHelper.readFileByLine(FILE_NAME);
+			List<String> newTrainList = new ArrayList<>();
+			boolean isRemoved = false;
+			for (String train : trainList) {
+				if (train.startsWith(id)) {
+					isRemoved = true;
+				} else {
+					newTrainList.add(train);
+				}
+			}
+			if (isRemoved) {
+				String updatedPayload = "";
+				for (int i = 0; i < newTrainList.size(); i++) {
+					updatedPayload += newTrainList.get(i);
+					if (i != newTrainList.size() - 1) {
+						updatedPayload += '\n';
+					}
+				}
+				DaoHelper.writeFile(updatedPayload, FILE_NAME);
+			}
+			return isRemoved;
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось удалить маршурт (ID - " + id + ". " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public List<Train> findAll() throws DaoException {
+		try {
+			return DaoHelper.readFileByLine(FILE_NAME).stream().map(this::convertFromString)
+					.collect(Collectors.toList());
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось получить список поездов. " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public List<Train> findAllByIds(Set<String> ids) throws DaoException {
+		try {
+			return DaoHelper.readFileByLine(FILE_NAME).stream().map(this::convertFromString)
+					.filter(train -> ids.contains(train.getId())).collect(Collectors.toList());
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось получить список поездов. " + ex.getMessage());
+		}
+	}
+
+	@Override
+	public List<Train> findByParameters(String from, String to, LocalDate date) throws DaoException {
+		try {
+			List<Train> trainList = DaoHelper.readFileByLine(FILE_NAME).stream().map(this::convertFromString).toList();
+			List<Train> filteredList = new ArrayList<>();
+			for (Train train : trainList) {
+				if (from != null && !train.getFrom().equals(from)) {
+					continue;
+				}
+				if (to != null && !train.getTo().equals(to)) {
+					continue;
+				}
+				if (date != null && !date.equals(train.getDepartureTime().toLocalDate())) {
+					continue;
+				}
+				filteredList.add(train);
+			}
+			return filteredList;
+		} catch (IOException ex) {
+			throw new DaoException("Не удалось получить список поездов. " + ex.getMessage());
+		}
+	}
+
+	private String prepareForStoring(Train train) {
+		return train.getId() + DELIMITER + train.getTrainNumber() + DELIMITER + train.getFrom() + DELIMITER
+				+ train.getTo() + DELIMITER + train.getDepartureTime() + DELIMITER + train.getFreeSeats() + DELIMITER
+				+ '\n';
+	}
+
+	private Train convertFromString(String trainStr) {
+		if (trainStr == null || trainStr.isEmpty())
+			return null;
+		Train train = new Train();
+		String[] split = trainStr.split(DELIMITER);
+		train.setId(split[0]);
+		train.setTrainNumber(Integer.parseInt(split[1]));
+		train.setFrom(split[2]);
+		train.setTo(split[3]);
+		train.setDepartureTime(LocalDateTime.parse(split[4]));
+		train.setFreeSeats(Integer.parseInt(split[5]));
+
+		return train;
+	}
+
+}
